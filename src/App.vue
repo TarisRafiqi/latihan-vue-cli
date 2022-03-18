56b71cecaf66df0fc0dc0ae44928973e10f64891
TarisRafiqi <taris.izatri18@student.uisi.ac.id>
2022-03-18 16:00:09 +0700

    Membuat latihan pada Vue CLI pada src/ App.vue

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+        <ol>
+            <li v-for="todo in todos" :key="todo.id">
+              {{ todo.text }}
+            </li>
+        </ol>
+
+        <input type="text" v-model="newTodo" @keyup.enter="addTodo">
+
+        <button @click="addTodo">Tambahkan</button>
+
+        <div v-if="todos.length >= 4">Hebat!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+
+  methods: {
+    addTodo() {
+      this.todos.push({ text: this.newTodo })
+      this.newTodo = ''
+    },
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: rgb(99, 99, 99)
+}
+</style>
